56f56680da889c896b93356fe3d4f9d009ca1f48
Naomi <naomi.t.voerman@gmail.com>
2018-09-11 12:42:23 +0200

    created empty file for cross-validation function

---
+++ cross_validation.py
@@ -0,0 +1,19 @@
+'''
+Created on Tue Sep 11 12:20:40 2018
+
+Script performs 5-fold cross-validation.
+'''
+
+import numpy as np
+
+# Split the data at random into 5-folds
+nfolds=5
+
+# Construct 5 models
+
+# Apply the models to the training and test sets
+#   generating predictions and calculation errors.
+
+# average erros over the training sets and over
+# the test sets to get estimates of the accuracy.
+# This is for both test and training sets.
