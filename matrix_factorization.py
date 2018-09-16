8ff1bf0c982fb279ce06041467b8cabce4245e40
Naomi <naomi.t.voerman@gmail.com>
2018-09-16 15:26:01 +0200

    filled in functions and algorithm. Not tested.

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -55,13 +55,14 @@ learn_rate = 0.005
 # initialize U and M
 U = np.empty([I, num_factors])
 M = np.empty([num_factors, J])
+print(np.dot(U, M))
 
 # apply the algorithm
 # loop until the terminal condition is met
 # that is when the RMSE does not decrease during two iterations
 def RMSE(x_actual, x_predicted):
     '''
-    Calculates the RMSE of two arrays.
+    Calculates the RMSE of two matrices.
 
     Arguments
     ---------
@@ -73,32 +74,85 @@ def RMSE(x_actual, x_predicted):
 
 def calculate_eij(x, x_pred):
     '''
+    Calculate the error for each element
+    and store in an numpy array.
+
+    Arguments
+    --------
+    x : np.array
+        This is an IxJ numpy array.
+    x_pred : np.array
+        This is an IxJ numpy array.
+
+    Return
+    -------
+    error : np.array
+        This is an IxJ numpy array.
     '''
-    error = x-x_pred
+    
+    error = np.subtract(x, x_pred)
     return error
 
 def calculate_gradient(error, m, u):
     '''
+    Calculate the gradient for each error
+    and store in an numpy array.
+
+    Arguments
+    --------
+    error : np.array
+        This is an IxJ numpy array.
+    m : np.array
+        This is an IxK numpy array.
+    u : np.array
+        This is an KxJ numpy array.
+
+    Return
+    -------
+    gradient_m : np.array
+        This is an ... numpy array.
+    gradient_u
+        This is an ... numpy array.
     '''
-    gradient_m = -2*error*m
-    gradient_u = -2*error*u
+
+    gradient_m = np.dot(-2*error, m.T)
+    gradient_u = np.dot(-2*error.T, u)
     return gradient_m, gradient_u
 
-def equation_6(u, eta, error, m, lr):
+def update_matrices(u, m, eta, lr, gradient_m, gradient_u, regularization):
     '''
-    '''
-    return u + eta * (2*error*m - lr*u)
 
-def equation_7(m, eta, error, u, lr):
+    Return
+    -------
+    U_update : np.array
+        This is an IxK array.
+    M_update : np.array
+        This is an KxJ array.
     '''
-    '''
-    return m + eta * (2*error*u - lr*m)
+    difference_u = np.subtract(-1 * gradient_u, regularization*u)
+    difference_m = np.subtract(-1 * gradient_m.T, regularization*m)
+
+    U_update = u + eta*difference_u
+    M_update = m + eta*difference_m
+    return U_update, M_update
 
 iter = 0
 while iter < num_iter:
+    # first calculate predicted X
+    X_predicted = np.dot(U, M)
+    RMSE = RMSE(X, X_predicted)
 
     # compute eij
+    error = calculate_eij(X, X_predicted)
+
     # compute the gradient of eij^2
+    gradient_m, gradient_u = calculate_gradient(error, M, U)
+
     # update the ith row of U and the jth column of M
+    U_update, M_update = update_matrices(U, M, learn_rate, gradient_m, gradient_u, regularization)
+
     # calculate the RMSE on the probe subset
+    X_predicted = np.dot(U_update, M_update)
+    RMSE_update = RMSE(X, X_predicted)
+
 
