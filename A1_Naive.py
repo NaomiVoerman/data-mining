a1a1842a10a739d9b05c5417cba8cb9169775b63
Naomi <naomi.t.voerman@gmail.com>
2018-09-23 14:48:08 +0200

    prints the average of CV

--- A1_Naive.py
+++ A1_Naive.py
@@ -13,17 +13,17 @@ movies.columns = ["MovieID","MovieTitle","Genre"]
 ratings.columns = ["UserID","MovieID","Rating","TimeStamp"]
 users.columns = ["UserID","Sex","AgeGroup","OccupationGroup","ZipCode"]
 
-# 1
+# 1. the global average
 def glb_avg(data, ind_train):
     global_average_rating = data.Rating[ind_train].mean(0)
-    rating_vec = np.repeat(global_average_rating,data.shape[0])
+    rating_vec = np.repeat(global_average_rating, data.shape[0])
     return(rating_vec)
     
 ratings.Rating.mean()
 
 global_avg_user = ratings.groupby(["UserID"]) [["Rating"]].mean()#()
 
-# 2
+# 2. the average per user
 def glb_avg_user(data,ind_train):
     global_avg_user = data[ind_train].groupby(["UserID"])[["Rating"]].mean() #.transform(mean)
     
@@ -39,11 +39,11 @@ def glb_avg_user(data,ind_train):
 #~1 in global_avg_user.index   
 global_avg_user.head(5)
 
-# 3
+# 3 the average per movie
 global_avg_movie = ratings.groupby(["MovieID"])[["Rating"]].mean()
 
-def glb_avg_movie(data,ind_train):
-    global_avg_mobie = data[ind_train].groupby(["MovieID"])[["Rating"]].mean() #.transform(mean)
+def glb_avg_movie(data, ind_train):
+    global_avg_movie = data[ind_train].groupby(["MovieID"])[["Rating"]].mean() #.transform(mean)
     
     #FallBack 
     rating_vec = glb_avg(data, ind_train)
@@ -69,8 +69,9 @@ np.random.shuffle(folds_vec)
 
 
 #initialize error vecs
-cols = ["global_train", "global_test", "user_train", "user_test", "movie_train", "movie_test", "combined_train", "combined_test"]
-err = pd.DataFrame(index = range(0,Nfolds), columns = cols)
+cols = ["global_train", "global_test", "user_train", "user_test", 
+    "movie_train", "movie_test", "combined_train", "combined_test"]
+err = pd.DataFrame(index = range(0, Nfolds), columns = cols)
 
 for fold in range(Nfolds):
     ind_test = folds_vec == fold
@@ -78,9 +79,9 @@ for fold in range(Nfolds):
     Ytest = ratings.Rating[ind_test]
     Ytrain = ratings.Rating[ind_train]
       
-    gobal_avg_vec = glb_avg(ratings,ind_train)
-    movie_avg_vec = glb_avg_movie(ratings,ind_train)
-    user_avg_vec = glb_avg_user(ratings,ind_train)
+    gobal_avg_vec = glb_avg(ratings, ind_train)
+    movie_avg_vec = glb_avg_movie(ratings, ind_train)
+    user_avg_vec = glb_avg_user(ratings, ind_train)
     
     #training errors
     err.global_train[fold] = np.sqrt(np.mean( (Ytrain - gobal_avg_vec[ind_train])**2))
@@ -97,35 +98,37 @@ for fold in range(Nfolds):
     #Design matrix
     X1 = user_avg_vec[ind_train]
     X2 = movie_avg_vec[ind_train]
-    X3 = np.repeat(1,len(X1))
-    X = np.vstack([X1,X2,X3]).T
+    X3 = np.repeat(1, len(X1))
+    X = np.vstack([X1, X2, X3]).T
     
-    betas = np.linalg.lstsq(X,Ytrain)[0]
+    betas = np.linalg.lstsq(X, Ytrain)[0]
     Ypred = np.dot(X, betas)
+    
     #improving Ypred
-    Ypred[Ypred>5] = 5
-    Ypred[Ypred<1] = 1
+    Ypred[Ypred > 5] = 5
+    Ypred[Ypred < 1] = 1
     
     #training error
-    err.combined_train[fold] = np.sqrt(np.mean( (Ytrain-Ypred)**2))
+    err.combined_train[fold] = np.sqrt(np.mean((Ytrain-Ypred)**2))
     
     #testing
     #Design matrix
     X1 = user_avg_vec[ind_test]
     X2 = movie_avg_vec[ind_test]
-    X3 = np.repeat(1,len(X1))
-    X = np.vstack([X1,X2,X3]).T
+    X3 = np.repeat(1, len(X1))
+    X = np.vstack([X1, X2, X3]).T
     Ypred = np.dot(X, betas)
+    
     #improving Ypred
     Ypred[Ypred>5] = 5
     Ypred[Ypred<1] = 1
 
     #test error
-    err.combined_test[fold] = np.sqrt(np.mean( (Ytest-Ypred)**2))
+    err.combined_test[fold] = np.sqrt(np.mean((Ytest - Ypred)**2))
     
     print("\nRMSE Fold " + str(fold) + ":")
     print(err.loc[fold])
     #print("Fold " + str(fold) + ": RMSE_train=" + str(err_train[fold]) + "; RMSE_test=" + str(err_test[fold]))
 
-err
-
+print(err)
+print(err.mean(0))
