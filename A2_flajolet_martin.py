bd9c14507b585da572e3e4b33fa939c1747abf65
Naomi <naomi.t.voerman@gmail.com>
2018-10-09 13:41:41 +0200

    Final FM

--- A2_flajolet_martin.py
+++ A2_flajolet_martin.py
@@ -16,14 +16,14 @@ between the number of "hashes"/"buckets", errors,
 amount of required memory as a function of the
 number of distinct elements in the stream.
 
-m = number of bitmaps
+m = the number of vectors in the bitmap
 k = number of buckets
 size = the size of the stream
 num_bits = the number of bits
 
 Apply serveral tests for:
 - N: size
-- M: num_bits
+- M: memory
 - RAE: ..
 '''
 
@@ -58,65 +58,85 @@ def trailing_zeroes(num, bits):
 
 # implement the algorithm for some
 # random properties
-k = 5
-m = 2**k
-size = 100
+ks = [4,5,6,7,8,9,10,11,12,13,14,15,16]
+sizes = [20**6]
 num_bits = 32
 
-# simulate data from which later on will be sampled again
-simulated_data = [random.getrandbits(num_bits) for i in range(size*100)]
-simulated_data = np.array(simulated_data)
-data = np.unique(simulated_data)
+# empty arrays to store results in
+RAEs = np.zeros([len(sizes), len(ks)])
+Estimates = np.zeros([len(sizes), len(ks)])
+Alphas = np.zeros([len(ks)])
 
 # simulate random 'num_bits'-data of length 'size'
-simulated_sequence = np.random.choice(data, size, replace=False)
-true_count = len(np.unique(simulated_sequence))
-bitmap = np.zeros([m, num_bits-k]) # this is a matrix of shape i*j
+streams = [[random.getrandbits(num_bits) for i in range(N)] for N in sizes]
+true_count = [len(list(set(stream))) for stream in streams]
 
-# fill in the bitmap as follows:
-#   take a hash-value:
-#       - idxi: use the first k bits (right to left)
-#         as index.
-#       - idxj: use the remainder to determine the
-#         trailing zeros.
+for j in range(len(streams)):
+  stream = streams[j]
 
-for s in range(size): # len(simulated_sequence)
-    # s: individual hash value
+  for k in range(len(ks)):
+    #print(ks[k])
+    m = 2**ks[k]
+    bitmap = np.zeros([m, (num_bits-ks[k])])
 
-    bin_bit = "{0:b}".format(simulated_sequence[s])
-    # determine the row index
-    rev = bin_bit[::-1]
-    idx = rev[0:k][::-1]
-    idxi = int(idx, 2)
+    # fill in the bitmap as follows:
+    #   take a hash-value:
+    #       - idxi: use the first k bits (right to left)
+    #         as index.
+    #       - idxj: use the remainder to determine the
+    #         trailing zeros.
 
-    # count trailing zeros of the remaining hash values
-    # this represents the column index.
-    hashv = int(bin_bit[0:k])
-    idxj = trailing_zeroes(hashv, num_bits-k)
-    
-    # assign the index of the bitmap to a 1
-    bitmap[idxi, idxj] = 1
+    for s in range(len(stream)):
+      # s: individual hash value
 
-# calculate R as follows:
-#   for every row in the bitmap:
-#   - if it only consists of 1's:
-#       row i = 0
-#   - else:
-#       give the index of where the
-#       first zero
+      bin_bit = "{0:b}".format(stream[s])
+      # determine the row index
+      rev = bin_bit[::-1]
+      idx = rev[0:ks[k]][::-1]
+      idxi = int(idx, 2)
 
-R = []
-for i in range(m):
-  # check which the indexes of each individual row 
-  # that is equal to zero.
-  r = np.where(bitmap[i, ] == 0)[0]
+      # count trailing zeros of the remaining binary's
+      # this represents the column index.
+      end = ks[k]
+      try:
+        hashv = int(bin_bit[0:-end])
+      except ValueError:
+        hasv = 0
+      idxj = trailing_zeroes(hashv, num_bits)
 
-  # take the smallest index
-  r_min = np.amin(r)
-  R.append(r_min)
+      # assign the index of the bitmap to a 1
+      bitmap[idxi, idxj] = 1
 
-phi = 0.77351 # Constant
-alpha = 0.78 / np.sqrt(m) # Estimated error
-est_count = m/phi * 2**(np.mean(R)) # Probabilistic_Counting error
-RAE = np.absolute(true_count - est_count)/true_count # Final Prediction Error
-print(RAE)
+    # calculate R as follows:
+    #   for every row in the bitmap:
+    #   - if it only consists of 1's:
+    #       row i = 0
+    #   - else:
+    #       give the index of where the
+    #       first zero
+
+    R = []
+    for i in range(m):
+      # check which the indexes of each individual row 
+      # that is equal to zero.
+      r = np.where(bitmap[i, ] == 0)[0]
+
+      # take the smallest index
+      r_min = np.amin(r)
+      R.append(r_min)   
+
+    phi = 0.77351
+    alpha = 0.78 / np.sqrt(m) # Estimated error
+    Alphas[k] = alpha
+    est_count = m/phi * 2**(np.mean(R)) # Probabilistic_Counting error
+    Estimates[j, k] = est_count
+    RAEs[j, k] = np.absolute(true_count[j] - est_count)/true_count[j] # Final Prediction Error
+
+print(true_count)
+print(Estimates)
+print(RAEs)
+print(Alphas)
+
+print(2.13962509e-01 == 0.213962509)
+print(4.36688484e-03 == 0.00436688484)
+print(1.00530251e+01 == 10.0530251)
