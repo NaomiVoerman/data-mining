9955f41fa88c0554e90684badb3c9fcdd1da7f1b
ksdejong <43182426+ksdejong@users.noreply.github.com>
2018-10-24 00:08:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 '''
 @Author: Koen de Jong & Naomi Voerman
-
 The goal is to find similar users of Netflix
 with help of minhasing and LSH.
-
 Minhashing converts large sets to short
 signatures, while preserving similarity.
 These are short integer vectors that 
 represent the sets and reflect their
 similarity.
-
 LSH focusses on pairs of signatures
 likely to be similar. Similarity is measured
 with Jaccard Similarity.
@@ -35,8 +32,9 @@ def combinations(mat):
     return(out)
 
 def Jsim(pair):
-    real_sim = np.unique(np.sum(np.hstack((Sparse_MU[:,pair[0]].toarray(),
-                                          Sparse_MU[:,pair[1]].toarray())) == [0.,0.], axis = 1),
+    #FUNTION HAS DEPENDENCY ON THE Sparse_MU_csc FROM OUTSIDE THE FUNCTION ENVIRONMENT!!!
+    real_sim = np.unique(np.sum(np.hstack((Sparse_MU_csc[:,pair[0]].toarray(),
+                                          Sparse_MU_csc[:,pair[1]].toarray())) == [0.,0.], axis = 1),
                         return_counts = True)
     
     indA = real_sim[0]==0
@@ -62,15 +60,19 @@ if __name__ == '__main__':
 
     user_movie = np.load(path)
 
-    Sparse_MU_coo = sc.coo_matrix((np.ones(user_movie.shape[0]), (user_movie[:,1], user_movie[:, 0])))
-    Sparse_MU = Sparse_MU_coo.tocsr()
+    Sparse_MU = sc.coo_matrix((np.ones(user_movie.shape[0]), (user_movie[:,1], user_movie[:, 0])))
+    Sparse_MU_csr = Sparse_MU.tocsr()
+    #Sparse_MU_csc = sc.csc_matrix((np.ones(user_movie.shape[0]), (user_movie[:,1], user_movie[:, 0])))
 
-    I = 60 # number of signatures
+    I = 80 # number of signatures
     M = np.ones((I, Sparse_MU.shape[1]))*np.Inf
     list_permutations = [np.random.permutation(Sparse_MU.shape[0]) for i in np.arange(I)]
 
+    #Minhasing
+    print("1. Minhashing: Constructing signature matrix M")
+    start = time.time()
     for i in np.arange(I):
-        Sparse_temp = Sparse_MU[list_permutations[i], :]
+        Sparse_temp = Sparse_MU_csr[list_permutations[i], :]
         
         for rowN in np.arange(Sparse_MU.shape[0]):
             ind_nonzero = Sparse_temp[rowN, :].nonzero()[1]
@@ -79,15 +81,17 @@ if __name__ == '__main__':
             
             if sum(M[i, :]) < np.Inf:
                 break
-
+    end = time.time()
+    print("   The time it takes to make signature matrix M is", end - start,"seconds")
     Sparse_MU_csr = None
     Sparse_temp = None
-    Sparse_MU_csc = Sparse_MU_coo.tocsc()
+    Sparse_MU_csc = Sparse_MU.tocsc()
 
     #Local sensitve hashing
     #To buckets:
-    k = 10
-    B = 6
+    print("2. Locale Sensitive hashing: Placing users in buckets per band")
+    k = 8
+    B = 10
     vec = 10**(np.arange(k))
     mat_extra = np.transpose(np.tile(vec, (M.shape[1], 1)))
     buckets = np.zeros((B, M.shape[1]))
@@ -96,6 +100,8 @@ if __name__ == '__main__':
         #buckets[i,:] = np.sum(M[i*k:(i*k+k), :], axis = 0)
 
     #Finding possible similar pairs from buckets:
+    print("3. Finding candidate pairs in buckets")
+    start = time.time()
     out = np.array((0, 0))
     for i in range(B):
         # find all the considered buckets: at least two movie-indices in bucket
@@ -107,18 +113,19 @@ if __name__ == '__main__':
             out = np.vstack((out, combinations(mat)))
     out = out[1:, :]
     out_uniq = np.unique(out, axis=0)
-    print(out_uniq.shape)
+    end = time.time()
+    print("   The time it takes to create the candidate pairs", end-start,"seconds")
 
     #Checking similarity in M
-    Mrow = M.shape[0]
-    ind_high_pos = np.array([sum(M[:,out_uniq[i,0]] == M[:,out_uniq[i,1]]) / Mrow for i in range(out_uniq.shape[0])]) > 0.5
-    print(ind_high_pos.shape)
+    print("Checking for similarity in signature matrix M")
+    ind_high_pos = np.array([sum(M[:,out_uniq[i,0]] == M[:,out_uniq[i,1]]) / I for i in range(out_uniq.shape[0])]) > 0.46
     #sum(np.array([sum(M[:,out_uniq[i,0]] == M[:,out_uniq[i,1]]) / Mrow for i in range(out_uniq.shape[0])]) > 0.5)
 
-    #Checking actual similarity in Sparse Matrix and writing to text
-    out_uniq = out_uniq[ind_high_pos, :]
-    out_uniq = np.sort(out_uniq, axis=1)
+    #Checking actual similarity in Sparse Matrix
+    print("Checking for similarity in actual sparse matrix")
+    out_uniq = out_uniq[ind_high_pos,:]
     for i in range(out_uniq.shape[0]):
+        out_uniq = np.sort(out_uniq, axis=1)
         sim = Jsim(out_uniq[i, :])
         if sim > 0.5:
             if i == 0:
@@ -129,6 +136,6 @@ if __name__ == '__main__':
                 f = open('results.txt', 'a')
                 f.write('\n' + str(out_uniq[i, 0])+','+str(out_uniq[i, 1]))
                 f.close()
-    
+ 
     total_end = time.time()
-    print("Total time:", total_end-total_start)
+print("Total time:", total_end-total_start)
